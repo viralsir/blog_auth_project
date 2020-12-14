2d199e18d8ea4f905b64dd1cb17349e10b833270
viralsir <viralsir2018@gmail.com>
2020-12-14 20:04:51 +0530

    set password_reset password functionality added into app

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    #fields = (('title','author'),('content','date_posted'))
+    fieldsets = (
+        ("Post Details", {
+            'fields': ('title', 'content')
+        }),("User Details", {
+            'fields': ('author', 'date_posted')
+        }))
+    search_fields = ['title']
+    list_display = ('title','content')
+
 # Register your models here.
-admin.site.register(Post)
+
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Post(models.Model):
     title=models.CharField(max_length=25)
     content=models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
+    date_posted=models.DateTimeField(default=timezone.now,verbose_name="Date")
     author=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
